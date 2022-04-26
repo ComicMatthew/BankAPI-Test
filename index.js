80a79c6bbe0f46a281a76c9b326b3e394edcc95c
ComicMatthew <mateusz.a.staniek@gmail.com>
2022-04-26 07:55:03 +0200

    CSS Edit

--- index.js
+++ index.js
@@ -1,50 +1,62 @@
+
+function insertText() {
+    console.log('Test')
+    
+
+}
+
+
 function showAllCurrency(){
 
-            let xhr = new XMLHttpRequest()
-            let table = "a"
-            let code = document.getElementById("curr").value
-            let date = document.getElementById("date").value
-            let url = `http://api.nbp.pl/api/exchangerates/tables/${table}/${date}/`
+    let xhr = new XMLHttpRequest()
+    let table = "a"
+    
+    let date = document.getElementById("date").value
+    let url = `http://api.nbp.pl/api/exchangerates/tables/${table}/${date}/`
 
 
-            xhr.open('GET', url, true)
+    xhr.open('GET', url, true)
 
-            xhr.onload = function() {
-                if (xhr.status == 200){
-                    console.log("success")
-                    let currency = JSON.parse(this.response)
-                    let ratess = currency[0].rates
-                    console.log(currency)
+    xhr.onload = function() {
+        
+        
+        
+        if (xhr.status == 200){
+            console.log("success")
+            let currency = JSON.parse(this.response)
+            let ratess = currency[0].rates
+            console.log(currency)
                     
-                    console.log(ratess)
+            console.log(ratess)
                     
-                    currency.forEach(element => {
-                        const dateCard = document.createElement('div')
-                        dateCard.setAttribute('id', 'date')
-                        dateCard.innerHTML = element.effectiveDate
-                        document.getElementById('feed').appendChild(dateCard)
-                    })
+             currency.forEach(element => {
+                 const dateCard = document.createElement('div')
+                 dateCard.setAttribute('id', 'date')
+                 dateCard.innerHTML = element.effectiveDate
+                 document.getElementById('feed').appendChild(dateCard)
+             })
                    
-                    ratess.forEach(element => {
-                        const currencyCard = document.createElement('div')
-                        currencyCard.innerHTML = element.currency
-                        document.getElementById('currency').appendChild(currencyCard)
+            ratess.forEach(element => {
+                const currencyCard = document.createElement('div')
+                currencyCard.innerHTML = element.currency
+                document.getElementById('currency').appendChild(currencyCard)
 
-                        const codeCard = document.createElement('div')
-                        codeCard.innerHTML = element.code
-                        document.getElementById('code').appendChild(codeCard)
+                const codeCard = document.createElement('div')
+                codeCard.innerHTML = element.code
+                document.getElementById('code').appendChild(codeCard)
 
-                        const midCard = document.createElement('div')
-                        midCard.innerHTML = element.mid
-                        document.getElementById('mid').appendChild(midCard)
-                    })
-                } else if (xhr.status == 404){
-                    alert("Nie pyklo!")
+                const midCard = document.createElement('div')
+                midCard.innerHTML = element.mid
+                document.getElementById('mid').appendChild(midCard)
+            })
+        } else if (xhr.status == 404){
+            alert("Brak danych z tego dnia")
                 
-                }
-            }
+        }
+    
+    }
 
-            xhr.send()
+    xhr.send()
 }
 
 
@@ -52,7 +64,7 @@ function showSpecificCurrency() {
 
         let xhr = new XMLHttpRequest()
         let table = "a"
-        let code = document.getElementById("curr").value
+        let code = document.getElementById("currInput").value 
         let date = document.getElementById("date").value
         let url = `http://api.nbp.pl/api/exchangerates/rates/${table}/${code}/${date}/`
 
@@ -79,7 +91,7 @@ function showSpecificCurrency() {
                 midCard.innerHTML = ratess[0].mid
                 document.getElementById('mid').appendChild(midCard)
             } else if (xhr.status == 404){
-                alert("Nie pyklo!")
+                alert("Brak danych z tego dnia")
             }
         } 
         xhr.send()
